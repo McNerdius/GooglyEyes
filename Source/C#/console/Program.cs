9f8935fabc364d3a2d665e8931c632694e380b44
McNerdius <mcnerd@techie.com>
2021-09-03 22:31:48 -0600

    tidy

--- Source/C#/console/Program.cs
+++ Source/C#/console/Program.cs
@@ -17,7 +17,9 @@ img.Save( imgStream, new PngEncoder() );
 imgStream.Seek( 0, SeekOrigin.Begin );
 var canvasImage = new CanvasImage( imgStream ).MaxWidth( 32 );
 
-var first = true;
+var firstReading = true;
+bool continuousMotion = true;
+
 var t = DateTime.Now;
 
 while ( true )
@@ -28,27 +30,30 @@ while ( true )
     AnsiConsole.Clear();
     AnsiConsole.Render( canvasImage );
 
-    var elapsed = (int) (DateTime.Now - t).TotalMilliseconds;
+    var elapsed = (float) (DateTime.Now - t).TotalSeconds;
     var scale = elapsed * TIME_SCALE;
 
-    #region "read" the "accelerometer"... 
+    if ( firstReading )
+    {
+        left.PupilVelocity = ReadAccelerometer();
+        right.PupilVelocity = ReadAccelerometer();
+    }
 
-    // simulate random X-axis jerks as would be seen on a 3D printer:
-    var (aX, aY) = (Fuzz(), 0f);
+    var (aX, aY) = ReadAccelerometer();
+    (aX, aY) = (aX * scale, (aY - GRAVITY) * scale);
 
-    //  - OR -
+    firstReading = false;
 
-    // an initial boost in a random direction, then let it settle
-    // var (aX, aY) = first ? (Fuzz( 4 ), Fuzz( 4 )) : (0f, 0f);
-    // first = false;
-
-    #endregion
-
-    aX *= scale;
-    aY *= scale;
-
-    left = left.Move( (aX, aY), DRAG );
-    right = right.Move( (aX, aY), DRAG );
+    left.Move( aX, aY );
+    right.Move( aX, aY );
 
     Thread.Sleep( 10 );
 }
+
+(float aX, float aY) ReadAccelerometer()
+=> (firstReading, continuousMotion) switch
+{
+    (true, _ ) => (Fuzz( 2 ), Fuzz( 2 )),
+    (false, true ) => (Fuzz( 0.75f ), 0f),
+    (false, false ) => (0f, 0f)
+};
