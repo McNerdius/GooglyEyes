852dd4b3bf62f82b060f9c4a7eeabcde18adae54
McNerdius <mcnerd@techie.com>
2021-09-05 17:04:31 -0600

    more specific color alias

--- Source/C#/console/GooglyEyeDrawExtensions.cs
+++ Source/C#/console/GooglyEyeDrawExtensions.cs
@@ -4,18 +4,18 @@ using SixLabors.ImageSharp.Drawing;
 using SixLabors.ImageSharp.Drawing.Processing;
 using SixLabors.ImageSharp.Processing;
 
-using Color = SixLabors.ImageSharp.Color;
+using ImageSharpColor = SixLabors.ImageSharp.Color;
 
 namespace GooglyEyes;
 
 public static class GooglyEyeDrawExtensions
 {
-    public static void Draw( this CanvasImage image, GooglyEye eye, int x = 0, bool clear = false )
+    public static void Draw( this CanvasImage image, GooglyEye eye, int x_offset = 0, bool clear = false )
     => image.Mutate( i =>
     {
         if ( clear )
-            i.Fill( Color.Black );
-        i.Fill( Color.White, new EllipsePolygon( eye.Radius + x, eye.Radius, eye.Radius ) );
-        i.Fill( Color.Black, new EllipsePolygon( eye.PupilPosition.X + eye.Radius + x, eye.Radius - eye.PupilPosition.Y, eye.PupilRadius ) );
+            i.Fill( ImageSharpColor.Black );
+        i.Fill( ImageSharpColor.White, new EllipsePolygon( eye.Radius + x_offset, eye.Radius, eye.Radius ) );
+        i.Fill( ImageSharpColor.Black, new EllipsePolygon( eye.PupilPosition.X + eye.Radius + x_offset, eye.Radius - eye.PupilPosition.Y, eye.PupilRadius ) );
     } );
 }
