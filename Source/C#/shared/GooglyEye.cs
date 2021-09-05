d7788b8c81ae9dd7e4375462ad1410afe0624163
McNerdius <mcnerd@techie.com>
2021-09-05 17:05:57 -0600

    tidy

--- Source/C#/shared/GooglyEye.cs
+++ Source/C#/shared/GooglyEye.cs
@@ -5,13 +5,13 @@ using static GooglyMath;
 
 public record GooglyEye( int Radius, int PupilRadius )
 {
-    public float InnerRadius = Radius - PupilRadius;
-    public float R2 = Pow( Radius - PupilRadius, 2 );
+    public int Diameter = Radius * 2;
+    public int PupilDiameter = PupilRadius * 2;
 
-    public float PupilDiameter = PupilRadius * 2;
+    public int InnerRadius = Radius - PupilRadius;
+    public int R2 = (int) Pow( Radius - PupilRadius, 2 );
 
     public (float X, float Y) PupilPosition { get; set; } = (0, 0);
-
     public (float X, float Y) PupilVelocity { get; set; } = (0, 0);
     public float TotalVelocity => SumOfSquares( PupilVelocity.X, PupilVelocity.Y );
 
