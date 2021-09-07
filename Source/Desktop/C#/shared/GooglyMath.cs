935fcecb75e92fdd5e004e2cbe9a234592175c16
McNerdius <mcnerd@techie.com>
2021-09-06 18:03:08 -0600

    LIS3DH returns short not float

--- Source/Desktop/C#/shared/GooglyMath.cs
+++ Source/Desktop/C#/shared/GooglyMath.cs
@@ -4,25 +4,21 @@ namespace GooglyEyes;
 
 public static class GooglyMath
 {
-    // keep this positive: will be subtracted
-    public static readonly float GRAVITY = 0.6f;
-
     // play with these, keep them >0 and <1
-    public static readonly float ELASTICITY = 0.95f;
-    public static readonly float DRAG = 0.996f;
+    public static readonly float ELASTICITY = 0.7f;
+    public static readonly float DRAG = 0.9f;
 
     // this will vary widely, depending on render speeds.
     // note this is ported from Arduino code for a 48mhz 
     // chip, and i hope to bring this code to .net nano
     // in the future, or i'd dump this scaling.
     // Faster chip or lower Thread.Sleep == lower scale
-    public static readonly float TIME_SCALE = 0.1f;
+    public static readonly float G_SCALE = 20f;
 
     private static Random fuzzGen = new();
 
-    // NextSingle() rarely returns zero
-    public static float Fuzz( float max = 1 )
-    => fuzzGen!.NextSingle() * max * (fuzzGen.Next( 3 ) - 1);
+    public static short Fuzz( short max )
+    => (short) (fuzzGen!.Next( max * 2 ) - max);
 
     public static float SumOfSquares( float a, float b ) => Pow( a, 2 ) + Pow( b, 2 );
     public static float Pythagorean( float a, float b ) => Sqrt( SumOfSquares( a, b ) );
