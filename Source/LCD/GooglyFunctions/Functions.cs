05a3a18ab45b12b149b9e10b1f0683a7b8ca35b7
McNerdius <mcnerd@techie.com>
2021-09-07 23:16:47 -0600

    logging and indentation

--- Source/LCD/GooglyFunctions/Functions.cs
+++ Source/LCD/GooglyFunctions/Functions.cs
@@ -19,46 +19,63 @@ public static class Functions
 {
     [FunctionName( "AddReading" )]
     public static async Task<IActionResult> AddReading(
-        [HttpTrigger( AuthorizationLevel.Anonymous, "get", "post", Route = null )] HttpRequest httpRequest,
+        [HttpTrigger( AuthorizationLevel.Anonymous, "get", "post" )] HttpRequest httpRequest,
         [Blob( "googly/readings.json", FileAccess.Read )] Stream blobInput,
         [Blob( "googly/readings.json", FileAccess.Write )] Stream blobOutput,
         ILogger log )
     {
-        var readings = blobInput switch
+        try
         {
-            null => Array.Empty<Reading>(),
-            _ => await JsonSerializer.DeserializeAsync<IEnumerable<Reading>>( blobInput )
-        };
+            var readings = blobInput switch
+            {
+                null => Array.Empty<Reading>(),
+                _ => await JsonSerializer.DeserializeAsync<IEnumerable<Reading>>( blobInput )
+            };
 
-        var reading = JsonSerializer.Deserialize<Reading>( await httpRequest.ReadAsStringAsync() );
-        var time = TimeZoneInfo.ConvertTimeFromUtc( DateTime.UtcNow, TimeZoneInfo.FindSystemTimeZoneById( "Mountain Standard Time" ) );
-        reading.Time = time.ToString();
+            var reading = JsonSerializer.Deserialize<Reading>( await httpRequest.ReadAsStringAsync() );
+            var time = TimeZoneInfo.ConvertTimeFromUtc( DateTime.UtcNow, TimeZoneInfo.CreateCustomTimeZone( "MST", TimeSpan.FromHours( -6 ), "MST", "MST" ) );
+            reading.Time = time.ToString();
 
-        readings = readings.Take( 100 ).Prepend( reading );
+            readings = readings.Take( 100 ).Prepend( reading );
 
-        await JsonSerializer.SerializeAsync( blobOutput, readings );
+            await JsonSerializer.SerializeAsync( blobOutput, readings, new JsonSerializerOptions { WriteIndented = true } );
 
-        return new OkResult();
+            return new OkResult();
+        }
+        catch ( Exception e )
+        {
+            log.LogError( e, e.Message );
+            return new EmptyResult();
+        }
     }
 
     [FunctionName( "GetReading" )]
     public static async Task<IActionResult> GetReading(
-    [HttpTrigger( AuthorizationLevel.Anonymous, "get", "post", Route = null )] HttpRequest _,
-    [Blob( "googly/readings.json", FileAccess.Read )] Stream blobInput )
+    [HttpTrigger( AuthorizationLevel.Anonymous, "get", "post" )] HttpRequest _,
+    [Blob( "googly/readings.json", FileAccess.Read )] Stream blobInput,
+    ILogger log )
     {
-        if ( blobInput is null )
+        try
         {
-            return new NotFoundResult();
-        }
-        else
-        {
-            var reading = (await JsonSerializer.DeserializeAsync<IEnumerable<Reading>>( blobInput )).First();
-
-            return new ContentResult
+            if ( blobInput is null )
             {
-                Content = JsonSerializer.Serialize( reading ),
-                ContentType = "application/json"
-            };
+                return new NotFoundResult();
+            }
+            else
+            {
+                var reading = (await JsonSerializer.DeserializeAsync<IEnumerable<Reading>>( blobInput )).First();
+
+                return new ContentResult
+                {
+                    Content = JsonSerializer.Serialize( reading ),
+                    ContentType = "application/json"
+                };
+            }
+        }
+        catch ( Exception e )
+        {
+            log.LogError( e, e.Message );
+            return new EmptyResult();
         }
     }
 }
